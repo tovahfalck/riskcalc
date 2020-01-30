aacaff9bcf2af333abba765663c8c5d0f99bbaa4
Tovah Falck <tovah.afalck@gmail.com>
2020-01-30 16:56:38 -0500

    started implementing the children method (calculates the children nodes -- not the grandchildren or great-grandchildren). first we had to teach the node how to decide on the number of red and white dice to use and then we started teaching it to decide on the number of outcomes for each combination and the likelihood of each outcome.

--- RiskGameOdds/RiskGameOdds/Node.swift
+++ RiskGameOdds/RiskGameOdds/Node.swift
@@ -18,8 +18,93 @@ class Node {
     }
     
     func children() -> [Result]{
-        //TODO: put logic for finding children
+        //1. determin the number of red dice
+        let numberOfRedDice = calculateNumberOfRedDice(numberOfPieces: attackers)
+        
+        //2. determin the number of white dice
+        let numberOfwhiteDice = calculateNumberOfWhiteDice(numberOfPieces: defenders)
+        
+        //3. chose the number of outcomes
+        switch numberOfRedDice {
+        case 3:
+            switch numberOfwhiteDice {
+            case 2:
+                return [
+                    Result (probability: 0.3717, outcome:
+                        Node(attackers: attackers, defenders: defenders - 2)),
+                    Result (probability: 0.3358, outcome:
+                        Node(attackers: (attackers - 1), defenders: (defenders - 1))),
+                    Result (probability: 0.2926, outcome:
+                        Node(attackers: attackers - 2, defenders: defenders)),
+                ]
+            case 1:
+                return [
+                    Result (probability: 0.6597, outcome:
+                        Node(attackers: attackers, defenders: defenders - 1)),
+                    Result (probability: 0.3403, outcome:
+                        Node(attackers: attackers - 1, defenders: defenders)),
+                ]
+            default:
+                <#code#>
+            }
+        case 2:
+            switch numberOfwhiteDice {
+            case 2:
+                <#code#>
+            case 1:
+                <#code#>
+            default:
+                <#code#>
+            }
+        case 1:
+            switch numberOfwhiteDice {
+            case 2:
+                <#code#>
+            case 1:
+                <#code#>
+            default:
+                <#code#>
+            }
+        default:
+            <#code#>
+        }
+        
+        
+        
         return [Result (probability: 0.293, outcome: Node(attackers: 3, defenders: 3))]
+    }
+    
+    /**
+     * We are assuming that the player will only choose to use the number of dice that gives them the highest odds.
+     */
+    func calculateNumberOfRedDice(numberOfPieces:Int) -> Int {
+        if (numberOfPieces >= 4){
+            return 3
+        }
+        if (numberOfPieces == 3){
+            return 2
+        }
+        if (numberOfPieces == 2){
+            return 1
+        }
+        else {
+            return 0
+        }
+    }
+    
+    /**
+     * We are assuming that the player will only choose to use the number of dice that gives them the highest odds.
+     */
+    func calculateNumberOfWhiteDice(numberOfPieces:Int) -> Int {
+        if (numberOfPieces >= 2){
+            return 2
+        }
+        if (numberOfPieces == 1){
+            return 1
+        }
+        else {
+            return 0
+        }
     }
 }
 
@@ -33,3 +118,4 @@ class Result {
         
     }
 }
+
--- RiskGameOdds/RiskGameOdds/ViewController.swift
+++ RiskGameOdds/RiskGameOdds/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var attackerNumber: UITextField!
+    @IBOutlet weak var defenderNumber: UITextField!
+    
+    @IBOutlet weak var resultAttackerWins: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func calculate(_ sender: Any) {
+        let attackers = attackerNumber.text
+        let defenders = defenderNumber.text
+        
+        let node = Node (attackers: Int(attackers!)!, defenders: Int(defenders!)!)
+        
+        //TODO: implement this correctly
+        
+        let result = node.children()[0]
+        resultAttackerWins.text = "\(String(format: "%.01f", result.probability * 100))%"
+    }
 }
 
